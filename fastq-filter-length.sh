a0c648b9b046e9d023d8932b7836a566b65505bc
Baraschr <bara.schramlova@icloud.com>
2020-11-29 18:00:58 +0100

    solution to ngs-course exercises

---
+++ fastq-filter-length.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# your_code_here
+
+echo Replace me with real code!
+echo Arguments: $1 $2
+
+# to stay with the 'tool concept'
+# expect input on stdin and output the results to stdout
